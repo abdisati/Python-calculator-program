79947c25cf1455aefefb9afc3314239c315e366a
Abdi Deresa <Abdishegute@gmail.com>
2024-08-26 13:32:53 -0700

    added label

--- file.py
+++ file.py
@@ -10,3 +10,21 @@ def equals():
 def clear():
     pass
 
+window = Tk()
+
+
+window.title("Calculator Program") #set the title
+window.geometry("500x500") #set the geometry of the window
+
+equation_text = ""   #set the an empty string
+
+equation_label = StringVar()
+
+label = Label(window,textvariable=equation_label, font=('consolas',20), bg="white", width=24, height=2)
+label.pack()
+
+
+
+
+
+window.mainloop()
